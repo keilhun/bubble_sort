e22b0c71ec1f9e43724cff60d02c9ddde37a012b
Keil Hunsaker <keilhun@gmail.com>
2015-09-19 15:59:16 -0400

    bubble_sort done, starting bubby_sort_by

---
+++ bubble_sort.rb
@@ -0,0 +1,27 @@
+def bubble_sort(to_be_sorted)
+  n = to_be_sorted.length
+  swapped = true
+  until swapped == false do
+    swapped = false
+    (1..n-1).each do |i|
+      if to_be_sorted[i-1] > to_be_sorted[i]
+        temp = to_be_sorted[i-1]
+        to_be_sorted[i-1] = to_be_sorted[i]
+        to_be_sorted[i] = temp
+        swapped = true
+      end
+    end
+  end
+  return to_be_sorted
+end
+
+def bubble_sort_by(to_be_sorted)
+
+end
+
+a = bubble_sort([4,76,9,32,67])
+print a
+
+a = bubble_sort_by (["hi", "hey", "hello"]) do |left, right|
+
+end
